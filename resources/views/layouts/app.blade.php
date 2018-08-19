05a24588dd424623b232a4849e4bd984ae141ad4
rdeos <rdeos@outlook.com>
2018-08-19 15:14:45 +0000

    WYSIWYG Editor

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,35 +1,38 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>@yield('title', 'LaraBBS') - Laravel 进阶教程</title>
+        <title>@yield('title', 'LaraBBS') - Laravel 进阶教程</title>
 
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-</head>
+        <!-- Styles -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        @yield('styles')
+    </head>
 
-<body>
-    <div id="app" class="{{ route_class() }}-page">
+    <body>
+        <div id="app" class="{{ route_class() }}-page">
 
-        @include('layouts._header')
+            @include('layouts._header')
 
-        <div class="container">
+            <div class="container">
 
-            @include('layouts._message')
-            @yield('content')
+                @include('layouts._message')
+                @yield('content')
 
+            </div>
+
+            @include('layouts._footer')
         </div>
 
-        @include('layouts._footer')
-    </div>
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}"></script>
+        @yield('scripts')
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
-</body>
+    </body>
 </html>
