2aa41e8d4d263a88ed11d62542bc2a89f7074bb3
rdeos <rdeos@outlook.com>
2018-08-21 16:35:42 +0000

    Integrate laravel-permission package

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -44,5 +44,11 @@ class UsersTableSeeder extends Seeder
         $user->avatar = 'https://rdeos.com/uploads/images/avatars/201808/13/1_1534093704_KmuiKLPluV.gif';
         $user->save();
 
+        // 初始化用户角色，将 1 号用户指派为『站长』
+        $user->assignRole('Founder');
+
+        // 将 2 号用户指派为『管理员』
+        $user = User::find(2);
+        $user->assignRole('Maintainer');
     }
 }
