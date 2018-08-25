542ffa0ae678537a1fed64e5158929e8694858d6
rdeos <rdeos@outlook.com>
2018-08-25 04:09:08 +0000

    replies index

--- routes/api.php
+++ routes/api.php
@@ -42,17 +42,27 @@ $api->version('v1', [
         'limit' => config('api.rate_limits.access.limit'),
         'expires' => config('api.rate_limits.access.expires'),
     ], function ($api) {
-        // 游客可以访问的接口
+        /* 游客可以访问的接口 */
+        // 话题分类
         $api->get('categories', 'CategoriesController@index')
             ->name('api.categories.index');
+        // 话题列表
         $api->get('topics', 'TopicsController@index')
             ->name('api.topics.index');
-        $api->get('users/{user}/topics', 'TopicsController@userIndex')
-            ->name('api.users.topics.index');
+        // 话题详情
         $api->get('topics/{topic}', 'TopicsController@show')
             ->name('api.topics.show');
+        // 话题回复列表
+        $api->get('topics/{topic}/replies', 'RepliesController@index')
+            ->name('api.topics.replies.index');
+        // 某个用户发布的话题
+        $api->get('users/{user}/topics', 'TopicsController@userIndex')
+            ->name('api.users.topics.index');
+        // 某个用户的回复列表
+        $api->get('users/{user}/replies', 'RepliesController@userIndex')
+            ->name('api.users.replies.index');
 
-        // 需要 token 验证的接口
+        /* 需要 token 验证的接口 */
         $api->group(['middleware' => 'api.auth'], function($api) {
             // 当前登录用户信息
             $api->get('user', 'UsersController@me')
